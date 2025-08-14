091dc8110875373a98e42f39dd022ef4991a6a2a
DennisJaspers <131969376+DennisJaspers@users.noreply.github.com>
2025-08-14 16:39:13 +0200

    Create app.js

---
+++ ci-cd/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (_, res) => {
+  res.send('Hello from Docker + GitHub Actions CI/CD!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
